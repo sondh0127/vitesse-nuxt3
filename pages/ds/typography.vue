629d512205f9a707ee05fd6517e318c8fb374f90
Son Hong Do <sondh0127@gmail.com>
2023-02-02 16:56:18 +0700

    typography

---
+++ pages/ds/typography.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>DemoTypographyClasses</h1>
+    <DemosTypographyDemoTypographyClasses />
+
+    <h1>DemoTypographyBasic</h1>
+    <DemosTypographyDemoTypographyBasic />
+  </div>
+</template>
